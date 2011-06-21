d5085ea29fad75e6cd8267ef135f442d01440776
Dugan <mike.dugan@theport.com>
2011-06-21 12:01:51 -0400

    Initial commit
    Signed-off-by: Dugan <mike.dugan@theport.com>

---
+++ RunE2E/Handler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Net;
+using System.IO;
+namespace RunE2E
+{
+    public class Handler
+    {
+
+        public void DoIt()
+        {
+            WebRequest oRequest = 
+                WebRequest.Create(
+                    @"http://crmdemo.rune2e.com/sap/bc/srt/rfc/sap/z_la_crm_lead/200/z_la_crm_lead/z_la_crm_lead");
+            oRequest.Method = "POST";
+            oRequest.ContentType = "text/xml";
+            NetworkCredential oCredentials = new NetworkCredential("theport", "theportis");
+            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+            oRequest.Credentials = oCredentials;
+            string RequestBody = 
+                @"
+                <soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:urn=""urn:sap-com:document:sap:soap:functions:mc-style"">
+                   <soapenv:Header/>
+                   <soapenv:Body>
+                      <urn:ZLaCrmLead>
+                <Function>0</Function>
+                      </urn:ZLaCrmLead>
+                   </soapenv:Body>
+                </soapenv:Envelope>
+                ";
+            byte[] bData = encoding.GetBytes(RequestBody);
+            oRequest.ContentLength = bData.Length;
+            Stream oStream = oRequest.GetRequestStream();
+            oStream.Write(bData, 0, bData.Length);
+            oStream.Close();
+            HttpWebResponse oResponse;
+            StreamReader oReader;
+            string sResponseBody;
+            try
+            {
+                oResponse = (System.Net.HttpWebResponse)oRequest.GetResponse();
+                oReader = new StreamReader(oResponse.GetResponseStream());
+                sResponseBody = oReader.ReadToEnd();
+            }
+            catch (System.Net.WebException we)
+            {
+                if (we.Response == null) { sResponseBody = we.ToString(); }
+                else
+                {
+                    HttpWebResponse oWebResponse = (System.Net.HttpWebResponse)we.Response;
+                    oReader = new StreamReader(we.Response.GetResponseStream());
+                    sResponseBody = oReader.ReadToEnd();
+                    //this.ResponseStatus = we.Response.htttStatus;
+                }
+            }
+            oRequest = null;
+            oResponse = null;
+            oReader = null;
+
+        }
+        
+
+
+    }
+}
---
+++ RunE2ECommon/Handler.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Collections;
+using System.Xml;
+
+namespace RunE2ECommon
+{
+    public class Handler
+    {
+
+        public void Main()
+        {
+            List<SoapRequest> oSoapRequests = GetSoapRequests();
+            foreach (SoapRequest oSoapRequest in oSoapRequests)
+            {
+                WebRequest oRequest =
+                WebRequest.Create(oSoapRequest.Url);
+                oRequest.Method = "POST";
+                oRequest.ContentType = "text/xml";
+                NetworkCredential oCredentials = new NetworkCredential("theport", "theportis");
+                System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+                oRequest.Credentials = oCredentials;
+                byte[] bData = encoding.GetBytes(oSoapRequest.RequestBody);
+                oRequest.ContentLength = bData.Length;
+                Stream oStream = oRequest.GetRequestStream();
+                oStream.Write(bData, 0, bData.Length);
+                oStream.Close();
+                HttpWebResponse oResponse;
+                StreamReader oReader;
+                string sResponseBody;
+                try
+                {
+                    oResponse = (System.Net.HttpWebResponse)oRequest.GetResponse();
+                    oReader = new StreamReader(oResponse.GetResponseStream());
+                    sResponseBody = oReader.ReadToEnd();
+                }
+                catch (System.Net.WebException we)
+                {
+                    if (we.Response == null) { sResponseBody = we.ToString(); }
+                    else
+                    {
+                        HttpWebResponse oWebResponse = (System.Net.HttpWebResponse)we.Response;
+                        oReader = new StreamReader(we.Response.GetResponseStream());
+                        sResponseBody = oReader.ReadToEnd();
+                    }
+                }
+
+                oRequest = null;
+                oResponse = null;
+                oReader = null;
+
+                XmlDocument oResponseXml = new XmlDocument();
+                oResponseXml.LoadXml(sResponseBody);
+            }
+        }
+
+
+        public void LogActivity(XmlDocument ActivityXml)
+        {
+            //Get the User to whom activities will be assigned.
+            WebRequest oUserRequest = WebRequest.Create(
+                @"http://services.theport.com/REST/V1/Users/1968941?devkey=dedabf54c268d476f2e75db49207ab91b0bb3503f479d444fb5dedea817da66a5954def58cf8cd1d");
+            oUserRequest.Method = "GET";
+            HttpWebResponse oUserResponse;
+            StreamReader oUserReader;
+            string sUserXml;
+            try
+            {
+                oUserResponse = (System.Net.HttpWebResponse)oUserRequest.GetResponse();
+                oUserReader = new StreamReader(oUserResponse.GetResponseStream());
+                sUserXml = oUserReader.ReadToEnd();
+            }
+            catch (System.Net.WebException we)
+            {
+                if (we.Response == null) { sUserXml = we.ToString(); }
+                else
+                {
+                    HttpWebResponse oWebResponse = (System.Net.HttpWebResponse)we.Response;
+                    oUserReader = new StreamReader(we.Response.GetResponseStream());
+                    sUserXml = oUserReader.ReadToEnd();
+                }
+            }
+            oUserReader = null;
+            oUserRequest = null;
+            oUserResponse = null;
+
+            XmlDocument oUserXml = new XmlDocument();
+            oUserXml.LoadXml(sUserXml.Replace(
+                @" xmlns=""http://services.theport.com"" xmlns:i=""http://www.w3.org/2001/XMLSchema-instance""",
+                ""));
+
+            string sLoggedActivities = oUserXml.SelectSingleNode("User")
+                .SelectSingleNode("CustomFields")
+                .SelectSingleNode("CustomField[ID=4814]")
+                .SelectSingleNode("Value").InnerText;
+            if (sLoggedActivities != "")
+            {
+                string[] aGuids = sLoggedActivities.Split(Convert.ToChar(","));
+                foreach (string sGuid in aGuids)
+                {
+                    //If we haven't yet logged the Guid of this particular entry
+                    if (0 == 1)
+                    {
+                        string sXml =
+                        @"
+                           <Post>
+                                  <Action>9</Action>
+                                  <Parameters>
+                                        <ActivityFeed>
+                                              <ActivityTypeID>46</ActivityTypeID>
+                                              <UserID>1968941</UserID>
+                                              <ActivityDate>" + System.DateTime.Now.ToString() + @"</ActivityDate>
+                                              <ActivityXml>" + ActivityXml.OuterXml + @"</ActivityXml>
+                                        </ActivityFeed>
+                                  </Parameters>
+                            </Post>
+                        ";
+                        WebRequest oRequest =
+                            WebRequest.Create(
+                            @"http://services.theport.com/REST/V1/Users?devkey=dedabf54c268d476f2e75db49207ab91b0bb3503f479d444fb5dedea817da66a5954def58cf8cd1d&sysuserid=1968941");
+                        oRequest.Method = "POST";
+                        oRequest.ContentType = "text/xml";
+                        System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+                        byte[] bData = encoding.GetBytes(sXml);
+                        oRequest.ContentLength = bData.Length;
+                        Stream oStream = oRequest.GetRequestStream();
+                        oStream.Write(bData, 0, bData.Length);
+                        oStream.Close();
+                        HttpWebResponse oResponse;
+                        StreamReader oReader;
+                        string sResponseBody;
+                        try
+                        {
+                            oResponse = (System.Net.HttpWebResponse)oRequest.GetResponse();
+                            oReader = new StreamReader(oResponse.GetResponseStream());
+                            sResponseBody = oReader.ReadToEnd();
+                        }
+                        catch (System.Net.WebException we)
+                        {
+                            if (we.Response == null) { sResponseBody = we.ToString(); }
+                            else
+                            {
+                                HttpWebResponse oWebResponse = (System.Net.HttpWebResponse)we.Response;
+                                oReader = new StreamReader(we.Response.GetResponseStream());
+                                sResponseBody = oReader.ReadToEnd();
+                            }
+                        }
+
+                        oRequest = null;
+                        oResponse = null;
+                        oReader = null;
+                    }
+                }
+            }
+        }
+
+        public static List<SoapRequest> GetSoapRequests()
+        {
+            List<SoapRequest> oRequests = new List<SoapRequest>();
+            SoapRequest oAccountRequest = new SoapRequest();
+            oAccountRequest.Url = "http://crmdemo.rune2e.com/sap/bc/srt/rfc/sap/z_la_crm_accounts/200/z_la_crm_accounts/z_la_crm_accounts";
+            oAccountRequest.RequestBody = @"
+                <soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:urn=""urn:sap-com:document:sap:soap:functions:mc-style"">
+                   <soapenv:Header/>
+                   <soapenv:Body>
+                      <urn:ZLaCrmAccounts>
+                         <Function>SRH</Function>
+                      </urn:ZLaCrmAccounts>
+                   </soapenv:Body>
+                </soapenv:Envelope>
+                ";
+            oAccountRequest.PayloadNode = "";
+
+            SoapRequest oActivitiesRequest = new SoapRequest();
+            oActivitiesRequest.Url = "http://crmdemo.rune2e.com/sap/bc/srt/rfc/sap/z_la_crm_actvities/200/z_la_crm_actvities/z_la_crm_actvities";
+            oActivitiesRequest.RequestBody = @"
+                <soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:urn=""urn:sap-com:document:sap:soap:functions:mc-style"">
+                   <soapenv:Header/>
+                   <soapenv:Body>
+                      <urn:ZLaCrmActivities>
+                        <Function>SRH</Function>
+                        <Guid>DE6FFE73DD7E44F19F97005056AB3A65</Guid>
+                      </urn:ZLaCrmActivities>
+                   </soapenv:Body>
+                </soapenv:Envelope>
+                ";
+            oActivitiesRequest.PayloadNode = "";
+
+            SoapRequest oLeadsRequest = new SoapRequest();
+            oLeadsRequest.Url = "http://crmdemo.rune2e.com/sap/bc/srt/rfc/sap/z_la_crm_lead/200/z_la_crm_lead/z_la_crm_lead";
+            oLeadsRequest.RequestBody = @"
+                <soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:urn=""urn:sap-com:document:sap:soap:functions:mc-style"">
+                   <soapenv:Header/>
+                    <soapenv:Body>
+                        <urn:ZLaCrmLead>
+                            <Function>SRH</Function>
+                            <Guid>DE70AD15BFA04AF19F97005056AB3A65</Guid>
+                        </urn:ZLaCrmLead>
+                    </soapenv:Body>
+                </soapenv:Envelope>
+                ";
+            oLeadsRequest.PayloadNode = "";
+
+            SoapRequest oProductsRequest = new SoapRequest();
+            oProductsRequest.Url = "http://crmdemo.rune2e.com/sap/bc/srt/rfc/sap/z_la_crm_products/200/z_la_crm_products/z_la_crm_products";
+            oProductsRequest.RequestBody = @"
+                <soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:urn=""urn:sap-com:document:sap:soap:functions:mc-style"">
+                      <soapenv:Header/>
+                    <soapenv:Body>
+                        <urn:ZLaCrmProducts></urn:ZLaCrmProducts>
+                    </soapenv:Body>
+                </soapenv:Envelope>
+                ";
+            oProductsRequest.PayloadNode = "";
+
+
+            SoapRequest oOpportunitiesRequest = new SoapRequest();
+            oOpportunitiesRequest.Url = "http://crmdemo.rune2e.com/sap/bc/srt/rfc/sap/z_la_crm_opportunity/200/z_la_crm_opportunity/z_la_crm_opportunity";
+            oOpportunitiesRequest.RequestBody = @"
+                <soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:urn=""urn:sap-com:document:sap:soap:functions:mc-style"">
+                      <soapenv:Header/>
+                   <soapenv:Body>
+                      <urn:ZLaCrmOpportunity><Function>SRH</Function></urn:ZLaCrmOpportunity>
+                   </soapenv:Body>
+                </soapenv:Envelope>
+                ";
+            oProductsRequest.PayloadNode = "";
+
+
+            oRequests.Add(oAccountRequest);
+            oRequests.Add(oActivitiesRequest);
+            oRequests.Add(oLeadsRequest);
+            oRequests.Add(oProductsRequest);
+            oRequests.Add(oOpportunitiesRequest);
+
+
+            return oRequests;
+        }
+    }
+
+    public class SoapRequest
+    {
+        public string Url { get; set; }
+        public string RequestBody { get; set; }
+        public string PayloadNode { get; set; }
+    }
+}
